dee91cb33d9121a4c18cf3bda3286ff05aa4f6f6
Atsushi Watanabe <atsushi.w@ieee.org>
2020-10-27 23:05:25 +0900

    Always close on keep alive error

--- reconnclient.go
+++ reconnclient.go
@@ -108,10 +108,10 @@ func (c *reconnectClient) Connect(ctx context.Context, clientID string, opts ...
 							); err != nil {
 								if cli, ok := c.Client().(*BaseClient); ok {
 									cli.SetErrorOnce(err)
-									// The client should close the connection if PINGRESP is not returned.
-									// MQTT 3.1.1 spec. 3.1.2.10
-									cli.Close()
 								}
+								// The client should close the connection if PINGRESP is not returned.
+								// MQTT 3.1.1 spec. 3.1.2.10
+								baseCli.Close()
 							}
 						}()
 					}
