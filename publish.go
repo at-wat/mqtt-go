9f5dd41fb3229e6c8cc04d3f3ded31479bbec296
Atsushi Watanabe <atsushi.w@ieee.org>
2021-02-15 12:49:08 +0900

    Fix publish retry

--- publish.go
+++ publish.go
@@ -209,7 +209,7 @@ func publishImpl(ctx context.Context, c *BaseClient, message *Message) error {
 			pktPubRel := (&pktPubRel{ID: message.ID}).Pack()
 			if err := c.write(pktPubRel); err != nil {
 				println("retry PUBREL queuing", message.ID)
-				return wrapErrorWithRetry(err, retryPublish, "sending PUBREL")
+				return wrapErrorWithRetry(err, retryPublish2, "sending PUBREL")
 			}
 			select {
 			case <-c.connClosed:
