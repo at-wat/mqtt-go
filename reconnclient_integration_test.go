a7e4e3c99670750c8bba44a60db196391cafd737
Atsushi Watanabe <atsushi.w@ieee.org>
2023-04-13 15:26:41 +0900

    Revert "Add wait"
    This reverts commit c3675df588badcddb74a54e704f821b865b62752.

--- reconnclient_integration_test.go
+++ reconnclient_integration_test.go
@@ -371,7 +371,17 @@ func TestIntegration_ReconnectClient_SessionPersistence(t *testing.T) {
 						t.Fatal("Timeout")
 					}
 
-					time.Sleep(100 * time.Millisecond)
+					done := make(chan struct{})
+					if err := cli.(*reconnectClient).RetryClient.pushTask(
+						ctx, func(ctx context.Context, cli *BaseClient) { close(done) },
+					); err != nil {
+						t.Fatal(err)
+					}
+					select {
+					case <-done:
+					case <-ctx.Done():
+						t.Fatal("Timeout")
+					}
 
 					cli.Disconnect(ctx)
 
