b169f094cd739787ce79a43aa17255bcdc1ef4a7
Atsushi Watanabe <atsushi.w@ieee.org>
2023-04-13 11:15:06 +0900

    Add debug log

--- reconnclient_integration_test.go
+++ reconnclient_integration_test.go
@@ -95,7 +95,11 @@ func TestIntegration_ReconnectClient(t *testing.T) {
 
 func newFilterBase(cbMsg func([]byte) bool) func([]byte) bool {
 	var readBuf []byte
+	var mu sync.Mutex
 	return func(b []byte) (ret bool) {
+		mu.Lock()
+		defer mu.Unlock()
+
 		readBuf = append(readBuf, b...)
 		ret = false
 		for {
@@ -266,8 +270,12 @@ func TestIntegration_ReconnectClient_SessionPersistence(t *testing.T) {
 							}
 							atomic.AddInt32(&dialCnt, 1)
 							ca, cb := filteredpipe.DetectAndClosePipe(
-								newFilterBase(func([]byte) bool { return false }),
 								newFilterBase(func(msg []byte) bool {
+									println(fmt.Sprintf("msg<- %x", msg))
+									return false
+								}),
+								newFilterBase(func(msg []byte) bool {
+									println(fmt.Sprintf("msg-> %x", msg))
 									if msg[0]&0xF0 == 0x80 {
 										atomic.AddInt32(&subCnt, 1)
 									}
@@ -290,6 +298,7 @@ func TestIntegration_ReconnectClient_SessionPersistence(t *testing.T) {
 
 					chReceived := make(chan *Message, 100)
 					cli.Handle(HandlerFunc(func(msg *Message) {
+						t.Logf("message: %v", msg)
 						chReceived <- msg
 					}))
 					_, err = cli.Connect(
@@ -325,7 +334,8 @@ func TestIntegration_ReconnectClient_SessionPersistence(t *testing.T) {
 						}
 					}
 					select {
-					case <-chReceived:
+					case msg := <-chReceived:
+						t.Logf("received: %v", msg)
 					case <-ctx.Done():
 						t.Fatal("Timeout")
 					}
@@ -350,7 +360,8 @@ func TestIntegration_ReconnectClient_SessionPersistence(t *testing.T) {
 						t.Fatalf("Unexpected error: '%v'", err)
 					}
 					select {
-					case <-chReceived:
+					case msg := <-chReceived:
+						t.Logf("received: %v", msg)
 					case <-ctx.Done():
 						t.Fatal("Timeout")
 					}
